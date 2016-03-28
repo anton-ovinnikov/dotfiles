3cb391c43420aba9f9f541664af359dd8a545b02
anton-ovinnikov <antono@bekitzur.com>
2016-03-28 12:11:50 +0300

    Update mchkout.sh

--- bin/mchkout.sh
+++ bin/mchkout.sh
@@ -9,7 +9,7 @@ PIPE="/opt/origami/connector-pipeline-python"
 PLAT="/opt/origami/platform-api-python"
 CONM="/opt/origami/connector-common-python"
 COMMON="/opt/origami/common-python"
-API="~/PycharmProjects/connector-api-python/"
+API="/home/user/PycharmProjects/connector-api-python/"
 
 REPOS=($PIPE $PLAT $CONM $COMMON $API)
 
@@ -17,5 +17,6 @@ for repo in ${REPOS[@]}; do
   cd $repo
   git stash
   git checkout $TARGET_BRANCH
-  git pull
+  git fetch
+  git reset --hard origin/"$TARGET_BRANCH"
 done

201fd05b0af8f51a0991f16676539edb9a164f57
Anton Ovinnikov <antono@bekitzur.com>
2016-04-02 00:26:46 +0300

    Removed that old shit

--- Git/windows.bat
+++
@@ -1,4 +0,0 @@
-:: config git with WinMerge
-git config --global diff.tool winmerge
-git config --global difftool.winmerge.cmd "C\:/Program\ Files\ \(x86\)/WinMerge/WinMergeU.exe -e -ub \"$LOCAL\" \"$REMOTE\""
-git config --global difftool.prompt false

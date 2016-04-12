cb824363da60324c7480ecb257b71bfbbe0c67a9
anton-ovinnikov <antono@bekitzur.com>
2016-04-12 16:48:47 +0300

    Update mchkout.sh

--- bin/mchkout.sh
+++ bin/mchkout.sh
@@ -3,36 +3,42 @@
 # set -x
 # trap read debug
 
-TARGET_BRANCH=$1
-FORSE=false
+TARGET_BRANCH='develop'
 
-API='/home/user/PycharmProjects/connector-api-python/'
-ING='/home/user/PycharmProjects/pipeline-ingest'
-PIPE='/home/user/PycharmProjects/connector-pipeline-python'
-PLAT='/home/user/PycharmProjects/platform-api-python'
-CONM='/home/user/PycharmProjects/connector-common-python'
+API='/home/anton/PycharmProjects/connector-api-python/'
+ING='/home/anton/PycharmProjects/pipeline-ingest'
+PIPE='/home/anton/PycharmProjects/connector-pipeline-python'
+PLAT='/home/anton/PycharmProjects/platform-api-python'
+CONM='/home/anton/PycharmProjects/connector-common-python'
 
 
 REPOS=($PIPE $PLAT $CONM $COMMON $API)
+
+
+while getopts ":t:f" opt; do
+	case "${opt}" in
+		f)
+			FORSE="true"
+			;;
+		t)
+			TARGET_BRANCH=$OPTARG
+			;;
+		:)
+			echo "Option -$OPTARG requires a parameter"
+			exit
+			;;
+		\?)
+			echo 'Bad option'
+			;; 
+	esac
+done
 
 for repo in ${REPOS[@]}; do
 	cd $repo
 	git stash
 	git checkout $TARGET_BRANCH
-	while getopts ":f" opt; do
-		case $opt in
-			f)
-				FORSE=true
-				git checkout $TARGET_BRANCH
-				;;
-			\?)
-				echo 'Bad option'
-				;; 
-		esac
-	done
-	if [ ! $FORSE ]
+	if [ "$FORSE" != "true" ]
 		then
-
 			git fetch
 			git reset --hard origin/"$TARGET_BRANCH"
 	fi
